9a0dc09ee345e6bdb65d9765f4d8966706f19b93
tnkjmalps <takuya-nakajima@alpsgiken.co.jp>
2013-12-14 11:16:35 +0900

    first commit

---
+++ src/test/java/citest/ProfileTest.java
@@ -0,0 +1,18 @@
+package citest;
+ 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+ 
+import org.junit.Test;
+ 
+public class ProfileTest {
+ 
+	@Test
+	public void testGetName() {
+		Profile profile = new Profile();
+		String expected = "じょん"; 
+		String actual = profile.getName();
+ 
+		assertThat(actual, is(expected));
+    }
+}
